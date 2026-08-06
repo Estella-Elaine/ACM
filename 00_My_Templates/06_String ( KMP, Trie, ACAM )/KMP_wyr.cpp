6008a3626c7ba57bd346c09341b3d5b679efd1e1
Estella-Elaine <yiranwang2007@gmail.com>
2026-08-06 21:35:57 +0800

    更新CP_Workspace文件

--- 00_My_Templates/06_String ( KMP, Trie, ACAM )/KMP_wyr.cpp
+++ 00_My_Templates/06_String ( KMP, Trie, ACAM )/KMP_wyr.cpp
@@ -10,12 +10,13 @@ const int inf = (1 << 30);
 const i64 INF = (1ll << 60);
 const double PI = acos(-1);
 
-void work(){
-	string s, p;
-	cin >> s >> p;
+// 返回 模式串p 在 文本串s 中所有匹配的 0-base 起点
+vector<int> kmpMatch(const string& s, const string& p){
+	int n = sz(p);
+	vector<int> nxt(n);
 
-	vector<int> nxt(sz(p)); // nxt[i]: p[0...i] 的最长相等真前后缀长度
-	for(int i = 1, j = 0; i < sz(p); ++i){
+	// nxt[i]: p[0...i] 的最长相等真前后缀长度
+	for(int i = 1, j = 0; i < n; ++i){
 		while(j && p[i] != p[j]) j = nxt[j - 1];
 		if(p[i] == p[j]) ++j;
 		nxt[i] = j;
@@ -25,21 +26,35 @@ void work(){
 	for(int i = 0, j = 0; i < sz(s); ++i){
 		while(j && s[i] != p[j]) j = nxt[j - 1];
 		if(s[i] == p[j]) ++j;
-		if(j == sz(p)){
-			pos.push_back(i - sz(p) + 1); // 0-base 匹配起点
-			j = nxt[j - 1]; // 继续寻找，可处理重叠匹配
+		if(j == n){
+			pos.push_back(i - n + 1);
+			j = nxt[j - 1]; // 继续匹配，支持重叠
 		}
 	}
 
+	return pos;
+}
+
+void work(){
+	string s, p;
+	cin >> s >> p;
+
+	// 题设1. 在 文本串s 中查找 模式串p 的所有出现位置
+	vector<int> pos = kmpMatch(s, p);
+
+	// 题设2. 只需判断是否有匹配
+	bool flag = !kmpMatch(s, p).empty();
+
 	cout << sz(pos) << '\n';
 	for(int i = 0; i < sz(pos); ++i){
-		cout << pos[i] << " \n"[i == sz(pos) - 1];
+		cout << pos[i] << " \n"[i == sz(pos) - 1]; // 0-base
 	}
 }
 
 signed main(){
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
+
 	int T = 1;
 	cin >> T;
 	while(T--) work();
